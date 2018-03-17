fc967a920fce849072f26844e5ad4ce14d8fa837
SHREYARAJ21 <30661154+SHREYARAJ21@users.noreply.github.com>
2018-03-17 21:46:57 +0530

    Add files via upload

---
+++ 6_smanum.py
@@ -0,0 +1,15 @@
+
+lower = 1
+upper = 100000
+
+
+print("Prime numbers between",lower,"and",upper,"are:")
+
+for num in range(lower,upper):
+   
+   if num > 1:
+       for i in range(2,num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
