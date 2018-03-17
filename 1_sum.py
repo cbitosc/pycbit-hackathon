dee30bf929bd04ea994907bd3e31fc4a2b8ea642
Mouli <srimouli1998@gmail.com>
2018-03-18 01:54:29 +0530

    Revert "s1 and s2"
    This reverts commit c99f7f58c86cbf2c6ce4146c43765395c9cea298.

--- 1_sum.py
+++
@@ -1,8 +0,0 @@
-def sumof500():
-    sum=0 #stores the sum of the numbers satisfying the below condition
-    for i in range(1,500):
-        if(i%5==0 and i%7!=0):
-            sum+=i
-    print ("sum is "+str(sum))
-sumof500()#function call 
-            
