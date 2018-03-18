30d811afc0c080ef6b4779d475c5f0899c6e3ef7
ChandraKanth_Reddy <30630380+jaichandrakanthreddy@users.noreply.github.com>
2018-03-18 21:41:21 +0530

    Update 6_prime.py

--- 6_prime.py
+++ 6_prime.py
@@ -6,5 +6,17 @@ for i in range(2,max+1):
             prime=False
     if prime:
         print(i)
+        
+        
+        
+        
+#python program for 4_pythogorean_triplet
+for a in range(1,1000):
+	for b in range(a+1,1000):
+		c=1000-(a+b)
+		if(a**2)+(b**2)==(c**2):
+			if a+b+c==1000:
+				print(a*b*c)
+				break
 
 
