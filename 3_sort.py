58a908b1ed85896403761ec6f6501ec2146845e5
harshitha2204 <30696277+harshitha2204@users.noreply.github.com>
2018-03-18 03:16:34 -0500

    Create 3_sort.py

---
+++ 3_sort.py
@@ -0,0 +1,6 @@
+list2=['Potter','Fred','Greg','George','Voldemort','Sirius','Dumbledore']
+list1=['Ron','Hermione','Harry','Professor','Dobby','The House Elf','Potter','Granger','Lockhart','Weasly']
+print(sorted(list1)+list2)
+
+
+
